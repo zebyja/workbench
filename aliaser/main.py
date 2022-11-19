c98772006add74ce535d2b80468835a97c7fceab
Milan Krafčišin <krafcisin@logmanager.cz>
2022-11-19 15:50:22 +0100

    wip completer

--- aliaser/main.py
+++ aliaser/main.py
@@ -1,2 +1,17 @@
+import readline
+
+commands = ["test"]
+
+
+def completer(text, state):
+    options = [i for i in commands if i.startswith(text)]
+    if state < len(options):
+        return options[state]
+    else:
+        return None
+
+
 def aliaser():
-    print('aliaser')
+    print("aliaser")
+    readline.parse_and_bind("tab: complete")
+    readline.set_completer(completer)
